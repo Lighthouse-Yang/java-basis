6bf8827df69b996bbe8e682bdbea1e9c2d07cc0b
PC <807490539@qq.com>
2019-08-26 18:57:23 +0800

    select

---
+++ src/three/select.java
@@ -0,0 +1,31 @@
+package three;
+
+/**
+ * Author : XiaoBa
+ * Time : 2019/8/26 14:27
+ * function:第三章-选择语句
+ * 选择就要用到条件,条件就是布尔表达式.
+ */
+import java.util.Scanner;
+
+public class select {
+    public static void main(String[] args){
+        //boolean数据类型:变量在比较过程中产生的结果为布尔值,True/False.
+        double radius=1;
+        System.out.println(radius>1);
+
+        //定义一个布尔变量.
+        boolean light=true;
+        System.out.println("布尔变量light:"+light);
+    }
+
+    // 案例：简单的计算器.
+    public static void calculation(){
+        int number1=(int)(System.currentTimeMillis() % 10);
+        int number2=(int)(System.currentTimeMillis() % 10);
+
+        //
+    }
+}
+
+
